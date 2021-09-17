bc2d3937d4fe8f88ccbc299b0bdb423a6f026e6a
dans77777 <61909995+dans77777@users.noreply.github.com>
2021-09-17 12:00:36 +0530

    Update addsalary.php

--- admin/addsalary.php
+++ admin/addsalary.php
@@ -8,17 +8,7 @@ if(!isset($_SESSION['legitEmail'])|| ($_SESSION['legitCDE'] != 'Admin')) {
 }
 $email= $_SESSION['legitEmail'];
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "intern";
-error_reporting(0);
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+
 
 $e=($_SESSION['legitEmail']);
 ?>
